6c0c48d83b3b94a024ef9b45191c731d6bac9b81
sumehra2004 <153430430+sumehra2004@users.noreply.github.com>
2024-12-11 07:11:57 +0530

    Create One_to_Hundred_PrimeNumbers.java

---
+++ One_to_Hundred_PrimeNumbers.java
@@ -0,0 +1,17 @@
+
+public class One_to_Hundred_PrimeNumbers {
+    public static void main(String[] args) {
+        for (int num = 2; num <= 100; num++) {
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(num);
+            }
+        }
+    }
+}
